e0af9ef3231a4825b92657deb28382f1f02332f5
tomatoman2 <kasuga01m@gmail.com>
2020-04-09 16:25:27 +0900

    fix front design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:edit, :update]
-    resources :messages, only: [:index, :create]
+  # resources :users, only: [:edit, :update]
+  #   resources :messages, only: [:index, :create]
 end
