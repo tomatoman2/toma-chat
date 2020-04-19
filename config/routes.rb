1950a620f76e59458bda3d9eb1dd65d26a8c8e75
tomatoman2 <kasuga01m@gmail.com>
2020-04-19 13:20:54 +0900

    fic routes function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:edit, :update, :index, :show]
   resources :messages do
     resources :comments, only: :create
   end
+  resources :users, only: [:edit, :update, :index, :show]
   resources :categories, only: [:show]
 end
